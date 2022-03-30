be8c2f90ae52b06d61d37aaebdf8d40ce64d059c
d-bryan <dylan.g.bryan@gmail.com>
2022-03-30 12:04:06 -0400

    added dev branch

---
+++ test-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test Branch</h1>
+      <p>New paragraph</p>
+    </div>
+  );
+}
+
+export default App;
